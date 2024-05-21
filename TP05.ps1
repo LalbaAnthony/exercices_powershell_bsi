f8a41175b91c7ecef955fab664e77efb2fe0954c
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-05-21 16:34:56 +0200

    TP 5 et 6

---
+++ TP05.ps1
@@ -0,0 +1,19 @@
+﻿# Script multiplicationTable.ps1
+
+# Demande à l'utilisateur d'entrer un nombre
+$number = Read-Host "Entrez un nombre pour afficher sa table de multiplication"
+
+# Vérifie que l'entrée de l'utilisateur est un nombre entier
+if ($number -match '^\d+$') {
+    # Convertir l'entrée en nombre entier
+    $number = [int]$number
+
+    # Afficher la table de multiplication de 1 à 10
+    Write-Output "Table de multiplication de $number :"
+    for ($i = 1; $i -le 10; $i++) {
+        $result = $number * $i
+        Write-Output "$number X $i = $result"
+    }
+} else {
+    Write-Output "Veuillez entrer un nombre entier."
+}
---
+++ TP06.ps1
@@ -0,0 +1,31 @@
+﻿# Script factorial.ps1
+
+# Demande à l'utilisateur d'entrer un nombre
+$number = Read-Host "Entrez un nombre pour calculer son factoriel"
+
+# Vérifie que l'entrée de l'utilisateur est un nombre entier positif
+if ($number -match '^\d+$' -and [int]$number -ge 0) {
+    # Convertir l'entrée en nombre entier
+    $number = [int]$number
+
+    # Fonction pour calculer le factoriel d'un nombre
+    function Get-Factorial {
+        param (
+            [int]$n
+        )
+        if ($n -le 1) {
+            return 1
+        } else {
+            return $n * (Get-Factorial -n ($n - 1))
+        }
+    }
+
+    # Calculer le factoriel
+    $factorial = Get-Factorial -n $number
+
+    # Afficher le résultat
+    Write-Output "Factoriel de $number"
+    Write-Output "$number! = $factorial"
+} else {
+    Write-Output "Veuillez entrer un nombre entier positif."
+}
